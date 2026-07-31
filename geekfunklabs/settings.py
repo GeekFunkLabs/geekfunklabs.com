2c55882fb13ca2d68c4664a054bfef17f46b9328
albedozero <albedozero@gmail.com>
2026-07-31 09:50:47 -0500

    set location for media and static files

--- geekfunklabs/settings.py
+++ geekfunklabs/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = env.get("DJANGO_STATIC_ROOT", BASE_DIR / "static")
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = env.get("DJANGO_MEDIA_ROOT", BASE_DIR / "media")
 
 # HTTPS
 
